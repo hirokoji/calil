1be3a83a2aeac9ad7d6dc9e1a6356c5dcafe3369
Hiro Kojima <guitarsaiko@gmail.com>
2020-06-15 07:53:14 +0900

    Update package.json and clean index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,2 @@
-#!/usr/bin/env node
-
-import {Calil} from "./Calil";
-import * as config from "../config.json";
-
-const main = async () => {
-
-    const calil = new Calil(config.SECRET_KEY);
-
-    const pref = '愛知県';
-    const cities = await calil.getAllCities(pref);
-    const city = cities['な'][1];
-
-    const libraries = await calil.getLibraries(pref, city);
-    const systemid = libraries[4].systemid;
-
-    const isbn = '4834000826';
-    const books = await calil.checkBookAvailability(isbn, systemid);
-    console.log(books);
-}
-
-main();
-
-// import {Cli} from "./Cli";
-// const rootDirPath = __dirname + '/../';
-// const cli = new Cli();
-// cli.cli(process.argv);
+export {Calil} from "./Calil";
 
