6419f0dae775562ce77810db7917fc1d66cdf3ec
Hiro Kojima <guitarsaiko@gmail.com>
2020-06-23 08:26:29 +0900

    Update lint and prettier setting

--- src/Calil.ts
+++ src/Calil.ts
@@ -1,57 +1,100 @@
 import axios, { AxiosResponse } from 'axios';
 
-
-export type Prefecture =  '北海道'| '青森県'| '岩手県'| '宮城県'| '秋田県'| '山形県'| '福島県'| '茨城県'| '栃木県'| '群馬県'| '埼玉県'| '千葉県' | '東京都' | '神奈川県'| '新潟県'| '富山県'| '石川県'| '福井県'| '山梨県'| '長野県'| '岐阜県'| '静岡県'| '愛知県'| '三重県'|
-    '滋賀県'| '京都府'| '大阪府'| '兵庫県'| '奈良県'| '和歌山県'| '鳥取県'| '島根県'| '岡山県'| '広島県'| '山口県'| '徳島県'| '香川県'| '愛媛県'| '高知県'| '福岡県'| '佐賀県'| '長崎県'| '熊本県'| '大分県'| '宮崎県'| '鹿児島県'| '沖縄県';
+export type Prefecture =
+  | '北海道'
+  | '青森県'
+  | '岩手県'
+  | '宮城県'
+  | '秋田県'
+  | '山形県'
+  | '福島県'
+  | '茨城県'
+  | '栃木県'
+  | '群馬県'
+  | '埼玉県'
+  | '千葉県'
+  | '東京都'
+  | '神奈川県'
+  | '新潟県'
+  | '富山県'
+  | '石川県'
+  | '福井県'
+  | '山梨県'
+  | '長野県'
+  | '岐阜県'
+  | '静岡県'
+  | '愛知県'
+  | '三重県'
+  | '滋賀県'
+  | '京都府'
+  | '大阪府'
+  | '兵庫県'
+  | '奈良県'
+  | '和歌山県'
+  | '鳥取県'
+  | '島根県'
+  | '岡山県'
+  | '広島県'
+  | '山口県'
+  | '徳島県'
+  | '香川県'
+  | '愛媛県'
+  | '高知県'
+  | '福岡県'
+  | '佐賀県'
+  | '長崎県'
+  | '熊本県'
+  | '大分県'
+  | '宮崎県'
+  | '鹿児島県'
+  | '沖縄県';
 
 export type BookStatus = {
-    libraryName: string;
-    status: "貸出可" |  "蔵書あり"| "館内のみ" | "貸出中" | "予約中" | "準備中"| "休館中" | "蔵書なし";
+  libraryName: string;
+  status: '貸出可' | '蔵書あり' | '館内のみ' | '貸出中' | '予約中' | '準備中' | '休館中' | '蔵書なし';
 };
 
 export class Calil {
-    private secretKey: string;
-    private format = "json";
+  private secretKey: string;
+  private format = 'json';
 
-    constructor(secretKey: string) {
-        this.secretKey = secretKey;
+  constructor(secretKey: string) {
+    this.secretKey = secretKey;
+  }
+
+  async checkBookAvailability(isbn: string, systemid: string): Promise<Error | BookStatus[]> {
+    const url = `http://api.calil.jp/check?appkey=${this.secretKey}&isbn=${isbn}&systemid=${systemid}&format=${this.format}&callback=no`;
+    const response: AxiosResponse = await axios.get(url);
+
+    if (response?.status !== 200) {
+      return new Error(`[Error] response status invalid. status code: ${response?.status}`);
     }
 
-    async checkBookAvailability(isbn: string, systemid: string): Promise<Error | BookStatus[]> {
-
-        const url = `http://api.calil.jp/check?appkey=${this.secretKey}&isbn=${isbn}&systemid=${systemid}&format=${this.format}&callback=no`;
-        const response: AxiosResponse = await axios.get(url);
-
-        if (response?.status !== 200) {
-            return new Error(`[Error] response status invalid. status code: ${response?.status}`);
-        }
-
-        if ( !response.data.books[isbn][systemid] || !response.data.books[isbn][systemid]['libkey'] ) {
-            return new Error(`[Error] libkey undefined.  Recived data from web api: ${JSON.stringify(response.data)}`);
-        }
-
-        const libkey = response.data.books[isbn][systemid]['libkey'];
-        const bookStatusList: BookStatus[] = Object.keys(libkey).map((libraryName: string) => {
-            return {libraryName, status: libkey[libraryName]};
-        });
-        
-
-        return bookStatusList
+    if (!response.data.books[isbn][systemid] || !response.data.books[isbn][systemid]['libkey']) {
+      return new Error(`[Error] libkey undefined.  Recived data from web api: ${JSON.stringify(response.data)}`);
     }
 
-    async getAllCities(pref: Prefecture){
-        const url = encodeURI(`http://api.calil.jp/citylist?pref=${pref}&format=json&callback=no`);
-        const response: AxiosResponse = await axios.get(url);
-        return response.data;
-        
-    }
+    const libkey = response.data.books[isbn][systemid]['libkey'];
+    const bookStatusList: BookStatus[] = Object.keys(libkey).map((libraryName: string) => {
+      return { libraryName, status: libkey[libraryName] };
+    });
 
-    async getLibraries(pref: Prefecture, city: string) {
+    return bookStatusList;
+  }
 
-        const url = encodeURI(`http://api.calil.jp/library?appkey=${this.secretKey}&format=json&callback=no&pref=${pref}&city=${city}`);
+  async getAllCities(pref: Prefecture) {
+    const url = encodeURI(`http://api.calil.jp/citylist?pref=${pref}&format=json&callback=no`);
+    const response: AxiosResponse = await axios.get(url);
+    return response.data;
+  }
 
-        const response: AxiosResponse = await axios.get(url);
-        const fixBrokenJson = () => response.data.replace(');', '');
-        return JSON.parse(fixBrokenJson());
-    }
+  async getLibraries(pref: Prefecture, city: string) {
+    const url = encodeURI(
+      `http://api.calil.jp/library?appkey=${this.secretKey}&format=json&callback=no&pref=${pref}&city=${city}`
+    );
+
+    const response: AxiosResponse = await axios.get(url);
+    const fixBrokenJson = () => response.data.replace(');', '');
+    return JSON.parse(fixBrokenJson());
+  }
 }
--- src/__tests__/Calil.test.ts
+++ src/__tests__/Calil.test.ts
@@ -4,37 +4,37 @@ import axios, { AxiosResponse, AxiosRequestConfig, AxiosInstance } from 'axios';
 jest.mock('axios');
 
 describe('Calil test', () => {
+  describe('check API', () => {
+    it(`should return {libraryName:'青県図' , status: '貸出可'}`, async () => {
+      const calil = new Calil(`SECRET_KEY`);
 
-    describe('check API', () => {
-        it(`should return {libraryName:'青県図' , status: '貸出可'}`, async () => {
-            const calil = new Calil(`SECRET_KEY`);
-
-            const isbn = '4834000826';
-            const library = 'Aomori_Pref';
-            const statusList = await calil.checkBookAvailability(isbn, library);
-            if(!(statusList instanceof Error)){
-                expect(statusList).toEqual([{libraryName:'青県図' , status: '貸出可'}]);
-            }
-        });
-
-        it(`should return error`, async () => {
-            const errorResponse =
-                {
-                    data: { "session": "89615da6c4ab2ab9555f1e51241765e7", "continue": 1, "books": { "4834000826": { "Aichi_Nagoya": { "status": "Running", "reserveurl": "" } } } },
-                    status: 200
-                };
-
-            const mockedAxios = axios as jest.Mocked<typeof axios>;
-            mockedAxios.get.mockResolvedValue(errorResponse);
-
-            const calil = new Calil(`SECRET_KEY`);
-
-            const isbn = '4834000826';
-            const library = 'Aomori_Pref';
-            const response = await calil.checkBookAvailability(isbn, library);
-            console.log(response);
-        })
-
+      const isbn = '4834000826';
+      const library = 'Aomori_Pref';
+      const statusList = await calil.checkBookAvailability(isbn, library);
+      if (!(statusList instanceof Error)) {
+        expect(statusList).toEqual([{ libraryName: '青県図', status: '貸出可' }]);
+      }
     });
 
+    it(`should return error`, async () => {
+      const errorResponse = {
+        data: {
+          session: '89615da6c4ab2ab9555f1e51241765e7',
+          continue: 1,
+          books: { '4834000826': { Aichi_Nagoya: { status: 'Running', reserveurl: '' } } }
+        },
+        status: 200
+      };
+
+      const mockedAxios = axios as jest.Mocked<typeof axios>;
+      mockedAxios.get.mockResolvedValue(errorResponse);
+
+      const calil = new Calil(`SECRET_KEY`);
+
+      const isbn = '4834000826';
+      const library = 'Aomori_Pref';
+      const response = await calil.checkBookAvailability(isbn, library);
+      console.log(response);
+    });
+  });
 });
